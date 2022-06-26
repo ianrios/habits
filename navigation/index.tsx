cc4101330700e69e4ec3068b90d9fa61377e859e
Ian Rios <ian.rios1a@gmail.com>
2022-06-26 16:05:53 -0400

    reverted packages and added new screens

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -14,8 +14,10 @@ import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
+import MainScreen from '../screens/MainScreen';
+import ActiveScreen from '../screens/ActiveScreen';
+import CompletedScreen from '../screens/CompletedScreen';
+import ArchivedScreen from '../screens/ArchivedScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -58,15 +60,15 @@ function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="main"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
       }}>
       <BottomTab.Screen
-        name="TabOne"
+        name="Main"
         component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+        options={({ navigation }: RootTabScreenProps<'Main'>) => ({
+          title: 'Main',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
@@ -85,10 +87,18 @@ function BottomTabNavigator() {
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="Active"
+        component={ActiveScreen}
         options={{
-          title: 'Tab Two',
+          title: 'Active',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Completed"
+        component={CompletedScreen}
+        options={{
+          title: 'Completed',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
